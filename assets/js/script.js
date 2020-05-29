3d2ebc0261a51f08be7a3de3d9db01bd05c409b1
gitShaun <wooshaun53@gmail.com>
2020-05-29 23:35:51 +0800

    Change duration to 2s in script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,5 +3,5 @@ const splash = document.querySelector('.splash');
 document.addEventListener('DOMContentLoaded', (e) => {
     setTimeout(() => {
         splash.classList.add('display-none');
-    }, 1700);
+    }, 2000);
 } )
